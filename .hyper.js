fc60d6d6f18a8b6385070f2fc3b0688fca5c44e1
Tomoya Hayashi <tomoya128@gmail.com>
2019-10-14 00:32:04 +0900

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily:  '"Meslo LG M DZ for Powerline", Menlo, Consolas, "Lucida Console", monospace',
+    fontFamily:  '"Roboto Mono Light for Powerline", Menlo, Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
